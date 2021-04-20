7bbda1a02dca0c45dce1bb4ef7923cacd1d7c8e2
connorHashDash <ckb1997@hotmail.co.uk>
2021-04-20 15:37:19 +0100

    game algorithms have been made and you can play 5 rounds of rock paper scissors against the computer will return when more knowledgeble and create a GUI

---
+++ README.md
@@ -0,0 +1,7 @@
+Simple game of rock paper scissors played through browser prompts to see the results of the game
+please look at the browser console via f12 
+
+I made this game to better understand javascript as I am currently learning, please let me know what
+you think!
+
+Connor :)
--- app.js
+++ app.js
@@ -1,13 +1,78 @@
-let answer = parseInt(prompt('gib number for buzzing and fizzing'))
 
-for (let i = 1; i <= answer; i++){
-    if (i % 5 === 0 && i % 3 === 0){
-        console.log('FizzBuzz!')
-    } else if (i % 3 === 0) {
-        console.log('fizz');
-    } else if (i % 5 === 0) {
-       console.log('buzz') 
-    } else
-    console.log(i)
+alert(`please open console (F12) to see the results of the game`)
+let userScore = 0
+let compScore = 0
+
+for(i = 0; i <= 5; i++){                                        //loops for five times
+
+console.log('You: ' + userScore)
+console.log('comp: '+ compScore)
+
+let userInput = prompt('rock paper or scissors?')               //asks user of rock paper or scissors
+let userPlay = function(){                                      //converts input to '1' '2' '3' 
+                    userInput.toLowerCase()
+                    if (userInput == 'rock'){
+                        return '1'
+                    } else if (userInput == 'paper'){
+                        return '2'
+                    } else if (userInput == 'scissors'){
+                       return  '3' 
+                    } else {
+                        return 0                                //returns 0 if invalid input is recieved 
+                    }
+    }
+ 
+
+function computerPlay(){                                        //gives randomly generated play from computer in '1' '2' or '3'
+    let randomN = Math.random();
+    if (randomN <= 0.33){
+        return '1'    
+    } else if (randomN >= 0.66){
+        return '2'
+    } else {
+        return '3'
+    }
 }
+
+
+let bothPlays = userPlay() + computerPlay()
+console.log(bothPlays)
+
+switch(bothPlays) {                                             //adds the two numbers together to decide winner/increment score
+    case '12':
+        ++compScore;
+        console.log('computer played paper, you lose!')
+    break;
+
+    case '13':
+        ++userScore
+        console.log('computer played scissors, you win!')
+    break
+
+    case '21':
+        ++userScore
+        console.log('computer played rock, you win!')
+    break
+
+    case '23':
+        ++compScore;
+        console.log('computer played scissors, you lose!')
+    break
+
+    case '31':
+        ++compScore;
+        console.log('computer played rock, you lose!')
+    break
+
+    case '32':
+        ++userScore
+        console.log('computer played paper, you win!')
+    break
+    default:
+        console.log(`Draw!`)
+}
+}
+console.log(`five rounds over final score
+you: ${userScore}
+comp ${compScore}`)
 
