b463b061eceb39c56f45f3af97cf0eebeca74bf2
connorHashDash <ckb1997@hotmail.co.uk>
2021-04-26 19:42:13 +0100

    added a basic gui and 5 round limit

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
+//DECLARING VARS
+let rock = document.querySelector('#rock')
+let paper= document.querySelector('#paper')
+let scissors = document.querySelector('#scissors')
 
-alert(`please open console (F12) to see the results of the game`)
+let uScoreP = document.createElement('p')
+let cScoreP = document.createElement('p')
+let gameInfo = document.createElement('p')
+
+let uScoreDiv = document.querySelector('.uScore')
+let cScoreDiv = document.querySelector('.cScore')
+let gameInfoDiv = document.querySelector('.gameInfo')
+
 let userScore = 0
 let compScore = 0
+let message 
 
-for(i = 0; i <= 5; i++){                                        //loops for five times
+//DECLARING FUNCTIONS
+function playRound(U, C) {
+let bothPlays = U + C;
 
-console.log('You: ' + userScore)
-console.log('comp: '+ compScore)
-
-let userInput = prompt('rock paper or scissors?')               //asks user of rock paper or scissors
-let userPlay = function(){                                      //converts input to '1' '2' '3' 
-                    userInput.toLowerCase()
-                    if (userInput == 'rock'){
-                        return '1'
-                    } else if (userInput == 'paper'){
-                        return '2'
-                    } else if (userInput == 'scissors'){
-                       return  '3' 
-                    } else {
-                        return 0                                //returns 0 if invalid input is recieved 
-                    }
-    }
- 
+switch(bothPlays) {                                             //adds the two numbers together to decide winner/increment score
+    case '12':
+        ++compScore;
+        message = 'computer played paper, you lose!'
+    break;
+    case '13':
+        ++userScore
+       message = 'computer played scissors, you win!'
+    break
+    case '21':
+        ++userScore
+        message = 'computer played rock, you win!'
+    break
+    case '23':
+        ++compScore;
+        message ='computer played scissors, you lose!'
+    break
+    case '31':
+        ++compScore;
+        message = 'computer played rock, you lose!'
+    break
+    case '32':
+        ++userScore
+        message = 'computer played paper, you win!'
+    break
+    default:
+        message = `Draw!`
+}
+}
 
 function computerPlay(){                                        //gives randomly generated play from computer in '1' '2' or '3'
     let randomN = Math.random();
@@ -34,45 +60,54 @@ function computerPlay(){                                        //gives randomly
     }
 }
 
-
-let bothPlays = userPlay() + computerPlay()
-console.log(bothPlays)
-
-switch(bothPlays) {                                             //adds the two numbers together to decide winner/increment score
-    case '12':
-        ++compScore;
-        console.log('computer played paper, you lose!')
-    break;
-
-    case '13':
-        ++userScore
-        console.log('computer played scissors, you win!')
-    break
-
-    case '21':
-        ++userScore
-        console.log('computer played rock, you win!')
-    break
-
-    case '23':
-        ++compScore;
-        console.log('computer played scissors, you lose!')
-    break
-
-    case '31':
-        ++compScore;
-        console.log('computer played rock, you lose!')
-    break
-
-    case '32':
-        ++userScore
-        console.log('computer played paper, you win!')
-    break
-    default:
-        console.log(`Draw!`)
+function endChecker(){
+if (userScore == 5) {
+    userScore = 0;
+    compScore = 0;
+    message =  `YOU WIN!!!!` 
+    uScoreP.textContent = `YOU: ${userScore}`
+    cScoreP.textContent = `COMP: ${compScore}`
+    gameInfo.textContent = `${message}`
+}else if (compScore== 5){
+    userScore = 0;
+    compScore = 0;
+    message =  `YOU LOSE HAHAHAHAHAHA!!!!` 
+    uScoreP.textContent = `YOU: ${userScore}`
+    cScoreP.textContent = `COMP: ${compScore}`
+    gameInfo.textContent = `${message}`
 }
 }
-console.log(`five rounds over final score
-you: ${userScore}
-comp ${compScore}`)
+//Buttons being pressed
+rock.addEventListener('click', function(){
+    let userPlay = '1'
+    playRound(userPlay, computerPlay())
+    uScoreP.textContent = `YOU: ${userScore}`
+    cScoreP.textContent = `COMP: ${compScore}`
+    gameInfo.textContent = `${message}`
+    endChecker()
+});
+paper.addEventListener('click', function(){
+    let userPlay = '2'
+    playRound(userPlay, computerPlay())
+    uScoreP.textContent = `YOU: ${userScore}`
+    cScoreP.textContent = `COMP: ${compScore}`
+    gameInfo.textContent = `${message}`
+    endChecker()
+});
+scissors.addEventListener('click', function(){
+    let userPlay = '3'
+    playRound(userPlay, computerPlay())
+    uScoreP.textContent = `YOU: ${userScore}`
+    cScoreP.textContent = `COMP: ${compScore}`
+    gameInfo.textContent = `${message}`
+    endChecker()
+});
+
+//Score Declaration
+uScoreP.textContent = `YOU: ${userScore}`
+cScoreP.textContent = `COMP: ${compScore}`
+uScoreDiv.appendChild(uScoreP)
+cScoreDiv.appendChild(cScoreP)
+gameInfoDiv.appendChild(gameInfo)
+
 
